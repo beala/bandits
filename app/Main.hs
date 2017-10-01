626c92aa2551e3759438cd33c54cc479b2a81b46
Alex Beal <alexlbeal@gmail.com>
2017-10-01 16:05:24 -0600

    implement regret function

--- app/Main.hs
+++ app/Main.hs
@@ -16,10 +16,8 @@ import Lens.Micro
 import Lens.Micro.Internal
 import Types
 
+-- Contraints that allow for random number generation
 type BanditM m = (MonadIO m, MonadReader (Rand.Gen (PrimState IO)) m)
-
-genNormalBandit :: Double -> Double -> Bandit Stat.NormalDistribution
-genNormalBandit avg stdDev = Bandit (Stat.normalDistr avg stdDev)
 
 -- |Generate a list of random bandits using dist to generate their parameters.
 randomBandits ::
@@ -29,65 +27,60 @@ randomBandits ::
   -> m [Bandit BanditDist]
 randomBandits count dist =
   replicateM count (randomBandit dist)
-
-randomBandit ::
-  BanditM m
-  => Stat.NormalDistribution
-  -> m (Bandit BanditDist)
-randomBandit d = do
-  gen <- ask
-  mean <- liftIO $ Stat.genContVar d gen
-  stdDev <- liftIO $ Stat.genContVar d gen
-  return $ genNormalBandit mean stdDev
+  where 
+    randomBandit d = do
+      gen <- ask
+      mean <- liftIO $ Stat.genContVar d gen
+      stdDev <- liftIO $ Stat.genContVar d gen
+      return $ genNormalBandit mean stdDev
+    genNormalBandit avg stdDev = Bandit (Stat.normalDistr avg stdDev)
 
 -- |Pulls the handle and returns the reward
-pullHandle ::
-  ( Stat.ContGen d
-  , MonadIO m
-  , MonadReader (Rand.Gen (PrimState IO)) m)
-  => Bandit d
-  -> m Double
-pullHandle (Bandit d) = do
-  gen <- ask
-  liftIO $ Stat.genContVar d gen
-
-pullHandleStats ::
-  ( MonadIO m
-  , MonadReader (Rand.Gen (PrimState IO)) m)
+pullArm ::
+  BanditM m
   => Bandit BanditDist
   -> m BanditStats
-pullHandleStats bandit = do
-  reward <- pullHandle bandit
-  return $ BanditStats reward
+pullArm (Bandit d) = do
+  gen <- ask
+  reward <- liftIO $ Stat.genContVar d gen
+  return $ BanditStats reward 1
 
 simulate ::
-  ( MonadIO m
-  , MonadReader (Rand.Gen (PrimState IO)) m)
-  => SimState
-  -> Int -- Number of rounds
-  -> ([BanditState] -> Traversal' [BanditState] (IxValue [BanditState]))
+  BanditM m
+  => SimState -- ^ Starting state
+  -> Int      -- ^ Number of rounds
+  -> ([BanditState] -> Traversal' [BanditState] (IxValue [BanditState])) -- ^ Choosing function
   -> m SimState
 simulate simState 0 _ = return $ simState
 simulate simState rounds f = do
   let options = simState ^. bandits
-  let Just (chosenStat, chosenBandit) = options ^? f options
-  reward <- pullHandleStats chosenBandit
-  let newState = simState & bandits . (f options) .~ (chosenStat `mappend` reward, chosenBandit)
-  let newState' = newState & roundsCount %~ (+ 1)
-  simulate newState' (rounds - 1) f
+  let Just (chosenStat, chosenBandit) = options ^? f options -- Pick a bandit
+  newStat <- pullArm chosenBandit
+  let newState = simState
+                 & bandits . (f options) . _1 .~ (chosenStat `mappend` newStat) -- Update stats
+                 & roundsCount %~ (+1) -- Update rounds count
+  simulate newState (rounds - 1) f
   
 initState ::
   BanditM m
   => Int -- Number of bandits
   -> m SimState
 initState count = do
-  initBandits <- randomBandits count (Stat.normalDistr 5 1)
+  initBandits <- randomBandits count (Stat.normalDistr 100 1)
   let initStats = replicate count mempty
   return $ SimState (zip initStats initBandits) 0
 
 regret :: SimState -> Double
-regret _ = 0.0
-
+regret simState =
+  let allTimesPulled = fromIntegral <$> (simState ^.. bandits . each . _1 . timesPulled)
+      allMeans = simState ^.. bandits . each . _2 . distLens . meanLens
+      expectedReturn = sum (zipWith (*) allTimesPulled allMeans)
+      maxMean = maximum (simState ^.. bandits . each . _2 . distLens . meanLens)
+      rounds = fromIntegral $ simState ^. roundsCount
+      maxExpectedReturn = maxMean * rounds
+  in
+    maxExpectedReturn - expectedReturn
+    
 run ::
   Int
   -> Int
--- src/Types.hs
+++ src/Types.hs
@@ -1,28 +1,35 @@
 {-# LANGUAGE TemplateHaskell #-}
+
 module Types where
 
 import Lens.Micro.TH
 import Lens.Micro
 import qualified Statistics.Distribution.Normal as Stat
+import qualified Statistics.Distribution as Stat
 
--- Represents a bandit.
--- `d` is the distribution
 data Bandit d = Bandit d
+
+distLens :: Lens' (Bandit d) d
+distLens = lens (\(Bandit d) -> d) (\_ d -> Bandit d)
+
+meanLens :: Lens' Stat.NormalDistribution Double
+meanLens = lens (\d -> Stat.mean d) (\d m -> Stat.normalDistr m (Stat.stdDev d))
 
 -- Statistics about a given bandit.
 data BanditStats = BanditStats { _totalReward :: Double
+                               , _timesPulled :: Int
                                } deriving (Eq, Show)
 
 makeLenses ''BanditStats
 
 instance Monoid BanditStats where
-  a `mappend` b = BanditStats (a ^. totalReward + b ^. totalReward)
-  mempty = BanditStats 0.0
+  a `mappend` b = BanditStats (a ^. totalReward + b ^. totalReward) (a ^. timesPulled + b ^. timesPulled)
+  mempty = BanditStats 0.0 0
 
 type BanditDist = Stat.NormalDistribution
 
-data SimState = SimState { _bandits :: [(BanditStats, Bandit BanditDist)] -- The bandits an associated stats
-                         , _roundsCount :: Int -- Number of rounds so far
+data SimState = SimState { _bandits :: [(BanditStats, Bandit BanditDist)] -- ^ The bandits and associated statistics
+                         , _roundsCount :: Int -- ^ Number of rounds so far
                          }
 
 makeLenses ''SimState
@@ -33,4 +40,3 @@ instance Show SimState where
       "{ bandits = " ++ show stats ++ ", n = " ++ show (simState ^. roundsCount) ++ "}"
 
 type BanditState = (BanditStats, Bandit BanditDist)
-

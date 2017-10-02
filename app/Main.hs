2edca562d0efa8a17c9b5a2532c5e1a79f04019a
Alex Beal <alexlbeal@gmail.com>
2017-10-02 08:05:15 -0600

    Add cli flags. Calculate regret during init.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,15 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 -- import Lib
@@ -15,6 +22,8 @@ import Control.Monad.Reader
 import Lens.Micro
 import Lens.Micro.Internal
 import Types
+import qualified Data.List as List
+import Options.Generic
 
 -- Contraints that allow for random number generation
 type BanditM m = (MonadIO m, MonadReader (Rand.Gen (PrimState IO)) m)
@@ -24,16 +33,16 @@ randomBandits ::
   BanditM m
   => Int
   -> BanditDist
+  -> BanditDist
   -> m [Bandit BanditDist]
-randomBandits count dist =
-  replicateM count (randomBandit dist)
+randomBandits count avgDist stdDevDist =
+  replicateM count randomBandit
   where 
-    randomBandit d = do
+    randomBandit = do
       gen <- ask
-      mean <- liftIO $ Stat.genContVar d gen
-      stdDev <- liftIO $ Stat.genContVar d gen
-      return $ genNormalBandit mean stdDev
-    genNormalBandit avg stdDev = Bandit (Stat.normalDistr avg stdDev)
+      mean <- liftIO $ Stat.genContVar avgDist gen
+      stdDev <- liftIO $ Stat.genContVar stdDevDist gen
+      return $ Bandit (Stat.normalDistr mean (abs stdDev))
 
 -- |Pulls the handle and returns the reward
 pullArm ::
@@ -49,55 +58,95 @@ simulate ::
   BanditM m
   => SimState -- ^ Starting state
   -> Int      -- ^ Number of rounds
-  -> ([BanditState] -> Traversal' [BanditState] (IxValue [BanditState])) -- ^ Choosing function
+  -> (SimState -> Traversal' SimState (IxValue [BanditState])) -- ^ Choosing function
   -> m SimState
-simulate simState 0 _ = return $ simState
+simulate simState 0 _ = return simState
 simulate simState rounds f = do
-  let options = simState ^. bandits
-  let Just (chosenStat, chosenBandit) = options ^? f options -- Pick a bandit
+  let Just (chosenStat, chosenBandit) = simState ^? f simState -- Pick a bandit
   newStat <- pullArm chosenBandit
   let newState = simState
-                 & bandits . (f options) . _1 .~ (chosenStat `mappend` newStat) -- Update stats
+                 & (f simState) . _1 .~ (chosenStat `mappend` newStat) -- Update stats
                  & roundsCount %~ (+1) -- Update rounds count
+                 & regretHist %~ (regret simState :)
   simulate newState (rounds - 1) f
-  
+
+-- | Initialize the state but creating the specified number of bandits and pulling each
+-- | one once.
 initState ::
   BanditM m
   => Int -- Number of bandits
   -> m SimState
 initState count = do
-  initBandits <- randomBandits count (Stat.normalDistr 100 1)
+  initBandits <- randomBandits count (Stat.normalDistr 0 5) (Stat.normalDistr 0 5)
   let initStats = replicate count mempty
-  return $ SimState (zip initStats initBandits) 0
+  return $ SimState (zip initStats initBandits) 0 []
 
 regret :: SimState -> Double
 regret simState =
   let allTimesPulled = fromIntegral <$> (simState ^.. bandits . each . _1 . timesPulled)
       allMeans = simState ^.. bandits . each . _2 . distLens . meanLens
-      expectedReturn = sum (zipWith (*) allTimesPulled allMeans)
+      actualExpectedReturn = sum (zipWith (*) allTimesPulled allMeans)
       maxMean = maximum (simState ^.. bandits . each . _2 . distLens . meanLens)
       rounds = fromIntegral $ simState ^. roundsCount
       maxExpectedReturn = maxMean * rounds
   in
-    maxExpectedReturn - expectedReturn
-    
+    maxExpectedReturn - actualExpectedReturn
+
 run ::
   Int
   -> Int
-  -> ([BanditState] -> Traversal' [BanditState] (IxValue [BanditState]))
-  -> ReaderT (Rand.Gen (PrimState IO)) IO SimState
+  -> (SimState -> Traversal' SimState (IxValue [BanditState]))
+  -> (ReaderT (Rand.Gen (PrimState IO)) IO) SimState
 run rounds count f = do
-  simState <- initState count
-  simulate simState rounds f
+  startingState <- initState count
+  simulate startingState rounds f
 
-choose :: [BanditState] -> Traversal' [BanditState] (IxValue [BanditState])
-choose _ = ix 1
+-- | Return the index of the largest item as determined by the function
+argMaxIndex :: (Ord b, Num b) => [a] -> (a -> b) -> Maybe Int
+argMaxIndex as f =
+  let magnitudes = f <$> as
+      indexAndMagnitude = zip [0..] magnitudes
+      biggestToSmallest = (List.sortOn (negative . snd) indexAndMagnitude)
+  in biggestToSmallest ^? _head . _1
+  where 
+    negative = (* (-1)) -- Swap the sign, so it's sorted biggest to smallest.
+
+choose :: SimState -> Traversal' SimState (IxValue [BanditState])
+choose simState =
+  let banditStats = simState ^.. bandits . each . _1
+      curRound = simState ^. roundsCount
+      Just maxIndex = argMaxIndex banditStats (\stats -> ucb stats curRound)
+  in
+  bandits . ix maxIndex
+
+ucb :: BanditStats -> Int -> Double
+ucb banditStats curRound  =
+  let totalTimesPulled = fromIntegral (banditStats ^. timesPulled)
+      empiricalMean = (banditStats ^. totalReward) / totalTimesPulled
+      t = fromIntegral curRound
+      confidence = sqrt ((2.0 * log (1 + (t * ((log t) ^ 2)))) / totalTimesPulled)
+      in
+    if totalTimesPulled /= 0 then empiricalMean + confidence else maxDouble
+  where
+    maxDouble = 100000000
 
 main :: IO ()
 main = do
+  (opts :: BanditOpts Unwrapped) <-unwrapRecord "Multi-armed bandit simulator."
   rand <- Rand.createSystemRandom
-  finalState <- runReaderT (run 1000 10 choose) rand
-  let totalRegret = regret finalState
-  putStrLn $ show finalState
-  putStrLn $ show totalRegret
+  finalState <- runReaderT (run (optRounds opts) (optBandits opts) choose) rand
+  -- _ <- traverse (putStrLn . show) (finalState ^.. bandits . each)
+  _ <- traverse (putStrLn . show) (reverse (finalState ^. regretHist))
+  return ()
   
+
+data BanditOpts w =
+  BanditOpts { optRounds :: w ::: Int <?> "Number of rounds to simulate."
+             , optBandits :: w ::: Int <?> "Number of bandits."
+             -- , optMeanMean :: w ::: Double <?> "Mean of the mean."
+             -- , optStdDevMean :: w ::: Double <?> "StdDev of the mean."
+             -- , optMeanStdDev :: w ::: Double <?> "Mean of the StdDev."
+             -- , optStdDevStdDev :: w ::: Double <?> "StdDev of the StdDev."
+             } deriving (Generic)
+
+instance ParseRecord (BanditOpts Wrapped)
--- src/Types.hs
+++ src/Types.hs
@@ -9,6 +9,9 @@ import qualified Statistics.Distribution as Stat
 
 data Bandit d = Bandit d
 
+instance (Stat.Mean d, Stat.Variance d) => Show (Bandit d) where
+  show (Bandit d) = "Bandit {mean = " ++ show (Stat.mean d) ++ ", stdDev = " ++ show (Stat.stdDev d) ++ "}"
+
 distLens :: Lens' (Bandit d) d
 distLens = lens (\(Bandit d) -> d) (\_ d -> Bandit d)
 
@@ -30,13 +33,12 @@ type BanditDist = Stat.NormalDistribution
 
 data SimState = SimState { _bandits :: [(BanditStats, Bandit BanditDist)] -- ^ The bandits and associated statistics
                          , _roundsCount :: Int -- ^ Number of rounds so far
+                         , _regretHist :: [Double]
                          }
 
 makeLenses ''SimState
 
 instance Show SimState where
-  show simState =
-    let stats = simState ^.. (bandits . each . _1) in
-      "{ bandits = " ++ show stats ++ ", n = " ++ show (simState ^. roundsCount) ++ "}"
+  show (SimState b count regretHist_) = "SimState { bandits = " ++ show b ++ ", n = " ++ show count ++ ", regret = "++ show regretHist_ ++"}"
 
 type BanditState = (BanditStats, Bandit BanditDist)
